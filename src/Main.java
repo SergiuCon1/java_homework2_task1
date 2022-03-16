9f5f1c8bfbb00ae25d0b0c64dfa5037d446c4df8
SergiuCon1 <sergiucontevici@gmail.com>
2022-03-16 22:19:22 +0200

    Create programm for bonus miles

---
+++ src/Main.java
@@ -0,0 +1,9 @@
+public class Main {
+    public static void main(String[] args) {
+        int priceTicket = 1200;
+        int oneBonusMiles = 20;
+
+        int countBonusMiles = (priceTicket/oneBonusMiles);
+        System.out.println("Количество бонусных милль = " + countBonusMiles);
+    }
+}
